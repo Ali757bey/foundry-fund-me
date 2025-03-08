b830df18cec96aac1a6a649a845258a32706691a
Ali Fletcher <ali757bey@gmail.com>
2025-03-08 09:58:34 -0500

    Updated submodule reference

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -34,17 +34,13 @@ contract HelperConfig is Script {
 
     function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
         // Price Feed Address
-        NetworkConfig memory ethConfig = NetworkConfig({
-            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
-        });
+        NetworkConfig memory ethConfig = NetworkConfig({priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419});
         return ethConfig;
     }
 
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
         // Price Feed Address
-        NetworkConfig memory sepoliaConfig = NetworkConfig({
-            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        });
+        NetworkConfig memory sepoliaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
         return sepoliaConfig;
     }
 
@@ -57,15 +53,10 @@ contract HelperConfig is Script {
         // 2. Return the address of the mock
 
         vm.startBroadcast();
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
-            DECIMALS,
-            INITIAL_ANSWER
-        );
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_ANSWER);
         vm.stopBroadcast();
 
-        NetworkConfig memory anvilConfig = NetworkConfig({
-            priceFeed: address(mockPriceFeed)
-        });
+        NetworkConfig memory anvilConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
         return anvilConfig;
     }
 }
